c692803272cba51aad48ed91736ebd55a8ec8466
iourt <appuebear@gmail.com>
2014-07-13 15:35:48 +0800

    add code

--- themes/js/app.js
+++ themes/js/app.js
@@ -1,1 +1,12 @@
-var app = {};
+var boxslider = {
+	init: function() {
+		var self = this;
+
+		var $slider = $('.boxslider');
+
+		alert($('.boxslider ul li').length)
+
+	}
+};
+
+boxslider.init();
